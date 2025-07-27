38d422dd8bef6f0d722e1eea327ca922ac05357e
mostakin5290 <mostakinmondal0842@gmail.com>
2025-07-27 19:37:54 +0530

    6

--- Backend/app.js
+++ Backend/app.js
@@ -21,7 +21,7 @@ const io = new Server(server, {
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true,
     },
-    // transports: ['polling', 'websocket'],
+    transports: ['polling', 'websocket'],
 });
 
 app.set('socketio', io);
